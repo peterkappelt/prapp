576b121d3bb5040691b378905f3cf372361dbf02
Peter Kappelt <kappelt.peter@gmail.com>
2024-03-01 19:15:04 +0000

    Workflow: validate that the commited OpenAPI spec matches the code

--- backend/prapp/settings_base.py
+++ backend/prapp/settings_base.py
@@ -2,11 +2,13 @@
 Base Django settings for prapp project.
 """
 
+import os
 from pathlib import Path
+
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
 
 # Application definition
 
